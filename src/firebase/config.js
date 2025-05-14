72a58007c7239e1343a6417c2f548dccd2b43fbb
Greenie5656 <andrewgreen5656@gmail.com>
2025-05-14 11:16:45 +0100

    Prepare clean version with .env setup

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,19 +3,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBtDUMuTpJPotI42CMUHLZsqC0MhaQXQAU",
-  authDomain: "events-platform-4657b.firebaseapp.com",
-  projectId: "events-platform-4657b",
-  storageBucket: "events-platform-4657b.firebasestorage.app",
-  messagingSenderId: "506762231683",
-  appId: "1:506762231683:web:c2ab97930725417abbc0b6"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-///Initialise Firebase
 const app = initializeApp(firebaseConfig);
-
-///Initialise Services
-
 const auth = getAuth(app);
 const db = getFirestore(app);
 
